80552b14f5e7161e1c8a56218655ae2046d7fbb9
Jens Låås <jens@jelaas.eu>
2023-02-21 09:08:46 +0100

    remove global struct

--- rundradio.c
+++ rundradio.c
@@ -11,11 +11,6 @@
 #include <arpa/inet.h>
 
 struct {
-	int fd_in, fd_out, maxfds;
-	struct pollfd *fds;
-} var;
-
-struct {
 	int verbose;
 	int count;
 	int lport, mport;
@@ -24,6 +19,8 @@ struct {
 int main(int argc, char **argv)
 {
 	struct sockaddr_in addr;
+	int fd_in, fd_out, maxfds;
+	struct pollfd *fds;
 	int i, j, one = 1;
 	ssize_t n;
 	char buf[128];
@@ -68,8 +65,8 @@ int main(int argc, char **argv)
 		argc--;
 	}
 
-	var.fds = calloc(conf.count, sizeof(struct pollfd));
-	if(!var.fds) exit(2);
+	fds = calloc(conf.count, sizeof(struct pollfd));
+	if(!fds) exit(2);
 
 	/* create two listening sockets. listen only on 127.0.0.1
 	   the first socket is for messenger(s)
@@ -78,56 +75,56 @@ int main(int argc, char **argv)
 	*/
 
 	/* socket for messengers to connect */
-	var.fd_in = socket( AF_INET, SOCK_STREAM, 0 );
-	setsockopt(var.fd_in, SOL_SOCKET, SO_REUSEADDR, (char *)&one, sizeof(one));
+	fd_in = socket( AF_INET, SOCK_STREAM, 0 );
+	setsockopt(fd_in, SOL_SOCKET, SO_REUSEADDR, (char *)&one, sizeof(one));
 	memset(&addr, 0, sizeof(addr));
 	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 	addr.sin_family = AF_INET;
 	addr.sin_port = htons(conf.mport);
-	if (bind(var.fd_in, (struct sockaddr*) &addr, sizeof(struct sockaddr_in)) < 0) {
+	if (bind(fd_in, (struct sockaddr*) &addr, sizeof(struct sockaddr_in)) < 0) {
 		return -1;
 	}
-	listen(var.fd_in,3);
+	listen(fd_in,3);
 
 	/* socket for listeners to connect */
-	var.fd_out = socket( AF_INET, SOCK_STREAM, 0 );
-	setsockopt(var.fd_out, SOL_SOCKET, SO_REUSEADDR, (char *)&one, sizeof(one));
+	fd_out = socket( AF_INET, SOCK_STREAM, 0 );
+	setsockopt(fd_out, SOL_SOCKET, SO_REUSEADDR, (char *)&one, sizeof(one));
 	memset(&addr, 0, sizeof(addr));
 	addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 	addr.sin_family = AF_INET;
 	addr.sin_port = htons(conf.lport);
-	if (bind(var.fd_out, (struct sockaddr*) &addr, sizeof(struct sockaddr_in)) < 0) {
+	if (bind(fd_out, (struct sockaddr*) &addr, sizeof(struct sockaddr_in)) < 0) {
 		return -1;
 	}
-	listen(var.fd_out,3);
+	listen(fd_out,3);
 
 	for(i=0;i<conf.count;i++) {
-		var.fds[i].fd = -1;
-		var.fds[i].events = POLLIN;
+		fds[i].fd = -1;
+		fds[i].events = POLLIN;
 	}
 
-	var.fds[0].fd = var.fd_in;
-	var.fds[1].fd = var.fd_out;
-	var.maxfds = 2;
+	fds[0].fd = fd_in;
+	fds[1].fd = fd_out;
+	maxfds = 2;
 
 	while(1) {
-		poll(var.fds, var.maxfds, -1);
+		poll(fds, maxfds, -1);
 
-		if(var.fds[0].revents) {
+		if(fds[0].revents) {
 			int fd;
 			/* set non-blocking on accepted connections so we never block in read or write */
-			fd = accept4(var.fds[0].fd, NULL, NULL, SOCK_NONBLOCK);
+			fd = accept4(fds[0].fd, NULL, NULL, SOCK_NONBLOCK);
 			if(fd >= 0) {
 				for(i=2;i<conf.count;i++) {
-					if(var.fds[i].fd == -1) {
-						var.fds[i].fd = fd;
+					if(fds[i].fd == -1) {
+						fds[i].fd = fd;
 						/* Set events to POLLIN|POLLPRI for messenger.
 						   We use POLLPRI only to keep track of which type of connection this is.
 						   This way we do not need any additional data structure to keep track of
 						   connection type.
 						 */
-						var.fds[i].events = POLLIN|POLLPRI;
-						if(i >= var.maxfds) var.maxfds = i+1;
+						fds[i].events = POLLIN|POLLPRI;
+						if(i >= maxfds) maxfds = i+1;
 						break;
 					}
 					if(i == (conf.count-1)) {
@@ -138,17 +135,17 @@ int main(int argc, char **argv)
 				}
 			}
 		}
-		if(var.fds[1].revents) {
+		if(fds[1].revents) {
 			int fd;
 			/* set non-blocking on accepted connections so we never block in read or write */
-			fd = accept4(var.fds[1].fd, NULL, NULL, SOCK_NONBLOCK);
+			fd = accept4(fds[1].fd, NULL, NULL, SOCK_NONBLOCK);
 			if(fd >= 0) {
 				for(i=2;i<conf.count;i++) {
-					if(var.fds[i].fd == -1) {
-						var.fds[i].fd = fd;
+					if(fds[i].fd == -1) {
+						fds[i].fd = fd;
 						/* Set events to POLLIN for listener */
-						var.fds[i].events = POLLIN;
-						if(i >= var.maxfds) var.maxfds = i+1;
+						fds[i].events = POLLIN;
+						if(i >= maxfds) maxfds = i+1;
 						break;
 					}
 					if(i == (conf.count-1)) {
@@ -160,26 +157,26 @@ int main(int argc, char **argv)
 			}
 		}
 		
-		for(i=2;i<var.maxfds;i++) {
-			if(var.fds[i].fd == -1) continue;
+		for(i=2;i<maxfds;i++) {
+			if(fds[i].fd == -1) continue;
 			
-			if(var.fds[i].revents == 0) continue;
+			if(fds[i].revents == 0) continue;
 
 			/* if we are a listener */
-			if(var.fds[i].events == POLLIN) {
+			if(fds[i].events == POLLIN) {
 				/* close listener on any event. they should always only listen */
-				close(var.fds[i].fd);
-				var.fds[i].fd = -1;
+				close(fds[i].fd);
+				fds[i].fd = -1;
 				continue;
 			}
 
 			/* if we are a messenger */
-			if(var.fds[i].events == (POLLIN|POLLPRI)) {
-				n = read(var.fds[i].fd, buf, sizeof(buf));
+			if(fds[i].events == (POLLIN|POLLPRI)) {
+				n = read(fds[i].fd, buf, sizeof(buf));
 				if(n == 0) {
 					/* end of file */
-					close(var.fds[i].fd);
-					var.fds[i].fd = -1;
+					close(fds[i].fd);
+					fds[i].fd = -1;
 					continue;
 				}
 				if(n == -1) {
@@ -188,13 +185,13 @@ int main(int argc, char **argv)
 					if(errno == EWOULDBLOCK) continue;
 				}
 				/* spread message to all listeners */
-				for(j=2;j<var.maxfds;j++) {
-					if(var.fds[j].events != POLLIN) continue;
+				for(j=2;j<maxfds;j++) {
+					if(fds[j].events != POLLIN) continue;
 					
 					/* close listener connection on any error when writing to listener */
-					if(write(var.fds[j].fd, buf, n) <= 0) {
-						close(var.fds[j].fd);
-						var.fds[j].fd = -1;
+					if(write(fds[j].fd, buf, n) <= 0) {
+						close(fds[j].fd);
+						fds[j].fd = -1;
 					}
 				}
 			}
